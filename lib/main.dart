504a5bd1dc14ecc57aa8a463f8a708f3bd57b23d
DESKTOP-8UO0TDJ\Pawel <pawel1998wp.pl@wp.pl>
2021-09-08 12:37:20 +0200

    with pop-up

--- lib/main.dart
+++ lib/main.dart
@@ -25,32 +25,54 @@ class GamePage extends StatefulWidget{
 
   @override
 class  _GamePageState extends State<GamePage>{
+  bool _alertIsVisible = false;
+
     @override
   Widget build(BuildContext context){
       return Scaffold(
         body: Center(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,,
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               Text(
-                "Hello MyApp",
+                "Hello new user",
                 style: TextStyle(fontWeight: FontWeight.bold, color: Colors.green),
               ),
               FlatButton(
                 child: Text('Please Hit me', style: TextStyle(color: Colors.blue)),
-                onPressed: () {},
+                onPressed: () {
+                  this._alertIsVisible = true;
+                  _showAlert(context);
+                  print("Button pressed");
+                },
               )
             ],
           ),
         )
       );
     }
-}
 
+    void _showAlert(BuildContext context){
+      Widget okButton = FlatButton(
+        child: Text("Great"),
+        onPressed: (){
+      Navigator.of(context).pop();
+      this._alertIsVisible = false;
+      print("Great  $_alertIsVisible");
+      });
+      showDialog(
+          context: context,
+          builder: (BuildContext context){
+            return AlertDialog(
+              title: Text("Hello there"),
+              content: Text("This is my first pop-up."),
+              actions: <Widget>[
+                okButton,
+              ],
+              elevation: 5,
+            );
 
-
-
-
-void main() {
-  runApp(MyApp());
+      },
+      );
+  }
 }
